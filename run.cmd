a5b756f71ddec32e0e80746ccec8fd7a47aafde1
thomaswoehlke <thomas.woehlke@gmail.com>
2020-01-20 13:25:13 +0100

    heroku

--- run.cmd
+++ run.cmd
@@ -1,10 +1,6 @@
 call setenv.cmd
 
-rem gradlew clean bootRun -Dspring.profiles.active=heroku
+gradlew clean bootRun
+rem gradlew clean bootJar
+rem java -jar build/libs/bloodmoney-1.0-SNAPSHOT.jar org.woehlke.bloodmoney.BloodMoneyApplication -Dspring.profiles.active=default
 
-gradlew clean bootRun 
-
-rem java -jar build/libs/bloodmoney-1.0-SNAPSHOT.jar org.woehlke.bloodmoney.BloodMoneyApplication -Dspring.profiles.active=heroku
-
-rem heroku login
-rem heroku local web
