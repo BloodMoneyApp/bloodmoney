c266c45b53553c4dc4d7ff1c8fe86b826defe94e
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-01-30 01:23:32 +0100

    clob

--- run.sh
+++ run.sh
@@ -5,15 +5,13 @@ source setenv.sh
 #./gradlew assemble
 
 function bootRunHeroku() {
-    #export SPRING_PROFILES_ACTIVE=heroku
     ./gradlew composeUp
-    ./gradlew clean bootRunHeroku
+    ./gradlew clean bootRun --args='--spring.profiles.active=heroku'
     ./gradlew composeDown
 }
 
 function bootRunDefault() {
-    #export SPRING_PROFILES_ACTIVE=default
-    ./gradlew clean bootRun
+    ./gradlew clean bootRun --args='--spring.profiles.active=default'
 }
 
 #bootRunHeroku
