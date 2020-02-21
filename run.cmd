cdb96bf936ffe0e86b0858dfa73f1fc25ec56a1b
thomaswoehlke <thomas.woehlke@gmail.com>
2020-02-21 12:18:57 +0100

    gradle wrapper update

--- run.cmd
+++ run.cmd
@@ -1,6 +1,10 @@
 call setenv.cmd
 
-gradlew clean bootRun
+cmd /c gradlew -i composeUp
+cmd /c gradlew -i clean bootRun --args='--spring.profiles.active=heroku'
+cmd /c gradlew -i composeDown
+
+rem gradlew clean bootRun
 rem gradlew clean bootJar
 rem java -jar build/libs/bloodmoney-1.0-SNAPSHOT.jar org.woehlke.bloodmoney.BloodMoneyApplication -Dspring.profiles.active=default
 
--- src/main/java/org/woehlke/bloodmoney/frontend/BloodPressureMeasurementResource.java
+++ src/main/java/org/woehlke/bloodmoney/frontend/BloodPressureMeasurementResource.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.woehlke.bloodmoney.application.BloodMoneyProperties;
 import org.woehlke.bloodmoney.frontend.model.UserSession;
@@ -15,6 +16,12 @@ import org.woehlke.bloodmoney.measurements.BloodPressureMeasurement;
 import org.woehlke.bloodmoney.measurements.BloodPressureMeasurementService;
 import org.woehlke.bloodmoney.user.UserSessionService;
 
+import javax.validation.Valid;
+
+/**
+ * http://localhost:5000/
+ * http://localhost:5000/api/measurement/all
+ */
 @Slf4j
 @RestController
 @RequestMapping("/api/measurement")
@@ -29,6 +36,29 @@ public class BloodPressureMeasurementResource {
     ) {
         model = userSessionService.handleUserSession(userSession, model);
         model.addAttribute("one", one);
+        return one;
+    }
+
+    @PutMapping("/{id}")
+    public BloodPressureMeasurement updateOne(
+        @PathVariable("id") BloodPressureMeasurement one,
+        @SessionAttribute(name="userSession",required=false) UserSession userSession,
+        Model model
+    ) {
+        model = userSessionService.handleUserSession(userSession, model);
+        model.addAttribute("one", one);
+        return one;
+    }
+
+    @PostMapping("/add")
+    public final BloodPressureMeasurement addPost(
+        @Valid BloodPressureMeasurement one,
+        @SessionAttribute(name="userSession", required=false) UserSession userSession,
+        BindingResult result, Model model
+    ) {
+        if(!result.hasErrors()){
+            one = bloodPressureMeasurementService.add(one);
+        }
         return one;
     }
 
