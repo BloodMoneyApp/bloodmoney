c459bf35e27f5d1a1372469db57920c684cf5bf7
thomaswoehlke <thomas.woehlke@gmail.com>
2020-03-08 11:54:34 +0100

    devops

--- src/test/java/org/woehlke/bloodmoney/measurements/BloodPressureMeasurementResourceTest.java
+++ src/test/java/org/woehlke/bloodmoney/measurements/BloodPressureMeasurementResourceTest.java
@@ -1,4 +1,43 @@
 package org.woehlke.bloodmoney.measurements;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@Slf4j
+@Getter
+@ActiveProfiles("dev")
+@SpringBootTest
 public class BloodPressureMeasurementResourceTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @PostConstruct
+    public void runBeforeAll() {
+        log.info("TEST: runBeforeAll");
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+        Assertions.assertNotNull(mockMvc,"runBeforeAll() context -> mockMvc");
+    }
+
+    @PreDestroy
+    void runAfterAll() {
+        log.info("TEST: runAfterAll");
+    }
 }
