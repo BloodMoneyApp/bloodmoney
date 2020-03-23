c793eea9e186acfa8239c5d4c00bad6ec68f2119
thomaswoehlke <thomas.woehlke@gmail.com>
2020-03-23 10:18:25 +0100

    bugfix docker

--- run.cmd
+++ run.cmd
@@ -14,8 +14,8 @@ cmd /c gradlew -i composeDown
 goto:END
 
 
-:bootRunPostgresSQL
-echo ---------------------------- bootRun PostgresSQL ------------------------------------------------
+:startBootRunPostgresSQL
+echo ---------------------------- start PostgresSQL ------------------------------------------------
 set BLOODMONEY_DEV_TESTING=false
 set SPRING_PROFILES_ACTIVE=%SPRING_PROFILES_ACTIVE_DEFAULT%
 set JAVA_OPTS=%JAVA_OPTS_DEFAULT%
@@ -26,13 +26,13 @@ cmd /c gradlew -i composeDown
 goto:END
 
 :testBootRunPostgresSQL
-echo ---------------------------- test H2 ------------------------------------------------
+echo ---------------------------- test PostgreSQL ------------------------------------------------
 set BLOODMONEY_DEV_TESTING=false
 set SPRING_PROFILES_ACTIVE=%SPRING_PROFILES_ACTIVE_DEFAULT%
 set JAVA_OPTS=%JAVA_OPTS_DEFAULT%
 cmd /c gradlew -i clean assemble bootJar
 cmd /c gradlew -i composeUp
-cmd /c gradlew -i clean assemble bootJar build test check
+cmd /c gradlew -i build test check
 cmd /c gradlew -i composeDown
 goto:END
 
