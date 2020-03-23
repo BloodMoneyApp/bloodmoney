35bb826f783a3d4217c7511093d451c506303977
thomaswoehlke <thomas.woehlke@gmail.com>
2020-03-23 09:46:09 +0100

    bugfix

--- run.cmd
+++ run.cmd
@@ -30,7 +30,10 @@ echo ---------------------------- test H2 --------------------------------------
 set BLOODMONEY_DEV_TESTING=false
 set SPRING_PROFILES_ACTIVE=%SPRING_PROFILES_ACTIVE_DEFAULT%
 set JAVA_OPTS=%JAVA_OPTS_DEFAULT%
+cmd /c gradlew -i clean assemble bootJar
+cmd /c gradlew -i composeUp
 cmd /c gradlew -i clean assemble bootJar build test check
+cmd /c gradlew -i composeDown
 goto:END
 
 :testH2
