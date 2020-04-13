fc3921559e73b0826d086ef187276de87fb76abc
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-13 18:29:45 +0200

    setup on Linux

--- etc/stop.sh
+++ etc/stop.sh
@@ -5,5 +5,5 @@ source setenv.sh
 export BLOODMONEY_DEV_TESTING=false
 export SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE_DEFAULT
 export JAVA_OPTS=$JAVA_OPTS_DEFAULT
-./gradlew -i composeDown
-./gradlew -i clean
+../gradlew -i composeDown
+../gradlew -i clean
