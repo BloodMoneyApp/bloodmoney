036f2f34c15a0864bfdb8b7033a4a1951ecde1ef
thomaswoehlke <thomas.woehlke@gmail.com>
2020-10-06 14:21:04 +0200

    setup to continue

--- build.sh
+++ build.sh
@@ -12,22 +12,22 @@ function composeDown() {
 
 function firstSetup() {
     export JAVA_OPTS=$JAVA_OPTS_RUN_DEFAULT
+    export TW_SKIP_TESTS='-DskipTests=true'
     showSettings
     ./mvnw dependency:purge-local-repository
-    ./mvnw -e -DskipTests=true clean dependency:resolve dependency:resolve-plugins dependency:sources dependency:tree
-    ./mvnw -e -DskipTests=true clean package spring-boot:repackage site
+    ./mvnw -e $TW_SKIP_TESTS clean dependency:resolve dependency:resolve-plugins dependency:sources dependency:tree
+    ./mvnw -e $TW_SKIP_TESTS clean package spring-boot:repackage site
 }
 
 function setupTravis() {
     export JAVA_OPTS=$JAVA_OPTS_RUN_DEFAULT
+    export TW_SKIP_TESTS='-DskipTests=true'
     showSettings
-    ./mvnw -e -DskipTests=true -B -V install -Dmaven.javadoc.skip=true && \
-    ./mvnw -e -DskipTests=true -B -V dependency:purge-local-repository && \
-    ./mvnw -e -DskipTests=true -B -V clean && \
-    ./mvnw -e -DskipTests=true -B -V dependency:resolve dependency:resolve-plugins dependency:sources && \
-    ./mvnw -e -DskipTests=true -B -V dependency:tree && \
-    ./mvnw -e -DskipTests=true -B -V clean package spring-boot:repackage && \
-    ./mvnw -e -DskipTests=true -B -V site
+    ./mvnw -e $TW_SKIP_TESTS -B -V install -Dmaven.javadoc.skip=true && \
+    ./mvnw -e $TW_SKIP_TESTS -B -V dependency:purge-local-repository clean && \
+    ./mvnw -e $TW_SKIP_TESTS -B -V dependency:resolve dependency:resolve-plugins dependency:sources dependency:tree && \
+    ./mvnw -e $TW_SKIP_TESTS -B -V clean package spring-boot:repackage && \
+    ./mvnw -e $TW_SKIP_TESTS -B -V site
 }
 
 function main() {
