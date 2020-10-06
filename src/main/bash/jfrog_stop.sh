8c856dc08a4521e798d6892f9b0911d994c4321f
thomaswoehlke <thomas.woehlke@gmail.com>
2020-10-06 14:15:31 +0200

    setup to continue

--- src/main/bash/jfrog_stop.sh
+++ src/main/bash/jfrog_stop.sh
@@ -1,1 +1,3 @@
-ps aux | grep java | grep '/opt/jfrog/artifactory/' | tr -s " " | cut -d" " -f2 | xargs sudo kill -TERM
+#!/usr/bin/env bash
+
+ps aux | grep java | grep -v 'JetBrains' | tr -s " " | cut -d" " -f2 | xargs sudo kill -TERM
