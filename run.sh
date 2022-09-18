3b36b762ced34deb4512c0ee6c7c144529530a76
thomaswoehlke <thomas.woehlke@gmail.com>
2022-09-18 21:33:41 +0200

    work

--- run.sh
+++ run.sh
@@ -2,6 +2,18 @@
 
 source src/main/bash/setenv.sh
 
+
+function herokuRun() {
+    ./mvnw -Pjar
+    JAVA_TOOL_OPTIONS=' -XX:+UseContainerSupport -Xmx300m -Xss512k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8'
+    JAVA_OPTIONS=JAVA_OPTIONS:$JAVA_TOOL_OPTIONS
+    java -jar target/bloodmoney.jar org.woehlke.bloodmoney.BloodMoneyApplication -Dspring.profiles.active=default
+}
+
+function run() {
+    ./mvnw
+}
+
 function main() {
     ## runHerokuLocal
     ## composeDown
@@ -14,6 +26,6 @@ function main() {
     runDev
 }
 
-main
+herokuRun
 
 
