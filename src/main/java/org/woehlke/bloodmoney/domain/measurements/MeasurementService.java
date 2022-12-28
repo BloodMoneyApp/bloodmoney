b198d23e9009d6209c2b75df141cc83a3aff4f8f
thomaswoehlke <thomas.woehlke@gmail.com>
2022-12-28 13:03:19 +0100

    spring security update

--- src/main/java/org/woehlke/bloodmoney/domain/measurements/MeasurementService.java
+++ src/main/java/org/woehlke/bloodmoney/domain/measurements/MeasurementService.java
@@ -1,7 +1,22 @@
 package org.woehlke.bloodmoney.domain.measurements;
 
-import org.woehlke.bloodmoney.application.EntityService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-public interface MeasurementService extends EntityService<MeasurementEntity,Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface MeasurementService {
+
+  List<MeasurementEntity> getAll();
+  Page<MeasurementEntity> getAll(Pageable pageable);
+  long count();
+
+  MeasurementEntity getOne(Long id);
+  Optional<MeasurementEntity> findById(Long id);
+
+  MeasurementEntity add(MeasurementEntity one);
+  MeasurementEntity update(MeasurementEntity one, Long id);
+  void delete(MeasurementEntity one);
+  void deleteAll();
 }
