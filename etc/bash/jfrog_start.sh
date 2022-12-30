bd9ff1a453217ecaf2bf584cab351a40ca40783b
thomaswoehlke <thomas.woehlke@gmail.com>
2022-12-30 10:31:16 +0100

    spring security update

--- etc/bash/jfrog_start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-ps aux | grep java | grep -v 'JetBrains' | tr -s " " | cut -d" " -f2 | xargs sudo kill -TERM
-
-sudo systemctl start postgresql
-sudo /opt/jfrog/artifactory/app/bin/artifactory.sh
--- etc/bash/jfrog_stop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ps aux | grep java | grep -v 'JetBrains' | tr -s " " | cut -d" " -f2 | xargs sudo kill -TERM
