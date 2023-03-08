25531a0e94ae3b7b7e36b80b522050b549da4c44
thomaswoehlke <thomas.woehlke@gmail.com>
2023-03-08 19:14:31 +0100

    tests

--- src/test/java/org/woehlke/bloodmoney/frontend/MeasurementEntityExportControllerTest.java
+++ src/test/java/org/woehlke/bloodmoney/frontend/MeasurementEntityExportControllerTest.java
@@ -9,12 +9,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+import static org.springframework.web.servlet.function.RequestPredicates.headers;
 
 //TODO #35 Test a Rest Controller with Spring Security
 @Slf4j
@@ -50,9 +58,16 @@ public class MeasurementEntityExportControllerTest {
         log.info("==================================================================================================");
     }
 
+    @WithMockUser(username = "thomas.woehlke@gmail.com")
     @Test
-    public void exportCSVTest() {
+    public void exportCSVTest() throws Exception  {
         log.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         log.info("TEST: exportCSVTest");
+        this.mockMvc.perform(get("/measurement/export/all"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("text/csv"))
+            .andExpect(content().string(containsString("CREATED;DATE;DIASTOLICBOTTOMNUMBER;HOSTNAME;HOSTNAMECANONICAL;IP;PULSE;SITUATION;SYSTOLICTOPNUMBER;TIME;UPDATED;WEIGHT")));
+        log.info("--------------------------------------------------------------------------------------------------");
     }
 }
